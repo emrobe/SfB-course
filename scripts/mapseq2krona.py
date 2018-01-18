86d273c273d3e5f0e7eb841cd49042f79a3774a8
Espen Mikal Robertsen <espen.robertsen@gmail.com>
2018-01-18 14:15:44 +0100

    Added filter functionality in mapseq2krona.py

--- scripts/mapseq2krona.py
+++ scripts/mapseq2krona.py
@@ -5,6 +5,7 @@ parser = argparse.ArgumentParser(description='This script converts Mapseq format
 parser.add_argument('input', help="Input file (Mapseq output file)", type=str)
 parser.add_argument('-c', type=float, default=0.5, help="Confidence score threshold. (default 0.5)")
 parser.add_argument('-u', type=int, default=0, help="Unclassified count. Mapseq does not provide information of unclassified reads, however if you have counted this manually, you can provide it for more sensible visualization in the Krona Chart")
+parser.add_argument('-f', action='store_true', help="Filter unassigned OTUs")
 args = parser.parse_args()
 
 data = collections.defaultdict(str)
@@ -26,8 +27,11 @@ with open(args.input) as handle:
 for k,v in data.items():
 	# Mapseq bug: The taxa B16S, and maybe others include an extra \t, which screws up strict x3 column output pr taxa while parsing. Perform quick fix.
 	if len(v)%3 != 0:
-		v.remove('')
-
+		if args.f:
+			index = v.index('')
+			v = v[:index]
+		else:
+			v.remove('')
 	for x, _ in enumerate(v):
 		if x%3 == 0:
 			if float(min(v[x+1], v[x+2])) >= args.c:
@@ -35,7 +39,6 @@ for k,v in data.items():
 					data[k] = [v[x]]
 				else:
 					data[k].append(v[x])
-
 # Print Krona format
 krona = collections.Counter(yield_all_lists(data))
 for k,v in krona.items():
